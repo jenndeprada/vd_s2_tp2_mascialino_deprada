8588a9cdfe907427903c1f09e32bd478939fffbb
jenndeprada <depradajennifer@gmail.com>
2023-03-31 11:31:49 -0300

    Guardado de cambios pre-merge con branch main

--- dataviz_1/script.js
+++ dataviz_1/script.js
@@ -93,6 +93,7 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
 		Plot.barX(arr, {
 			x: "hs_mision_por_pais",
 			y: "pais",
+			fill: "#a07dfe"
 			//fill: data.map(d => d.ocupacion),  
 			//title: "ocupacion",
 			//sort: "ocupacion",  
